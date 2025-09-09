33e04b2d8ed27e159ed0de0b1ca24aaaa6af6a06
Hirushan <kavinduhirushan78@gmail.com>
2025-09-09 13:19:49 +0530

    Add run budil

--- backend/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createUser,loginUser } from "../controller/useController.js";
-
-const userRoute = express.Router();
-
-userRoute.post("/", createUser)
-userRoute.post("/login",loginUser)
-
-export default userRoute;
